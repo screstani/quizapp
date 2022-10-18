02b11de4c38abb606325bb9e7e702e043d990731
Sayonara <sayonara.crestani@gmail.com>
2022-10-18 17:13:37 -0300

    includes the score keeper and the question counter git status uit exti exit cd ..

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -60,7 +62,6 @@ startGame = () => {
     // spread operator, the 3 dots, spread each of the itens in the questions into a new array
     // we copy so we don't change the original array
     availableQuestions = [ ... questions];
-    console.log(availableQuestions);
     getNewQuestion();
 };
 
@@ -71,6 +72,8 @@ if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     return window.location.assign("end.html");
 }
     questionCounter ++;
+    questionCounterText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -94,10 +97,27 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+       
+        //ternary sintax that compares the answers
+        //a comparação é entre string e number, entào double equals
+        const classToApply = selectedAnswer==currentQuestion.answer ? "correct" : "incorrect";
+        
+        if(classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
     });
 });
 
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 
 startGame();
