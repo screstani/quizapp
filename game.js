c490f1bae543111695ebe4cd0c1787b05868f877
Sayonara <sayonara.crestani@gmail.com>
2022-10-19 17:34:24 -0300

    changes local json for OpenTrivia API

--- game.js
+++ game.js
@@ -1,57 +1,51 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "What was the first capital of Italy?",
-        choice1: "Roma",
-        choice2: "Torino",
-        choice3: "Milano",
-        choice4: "Venezia",
-        answer: 2    
-    },
-    {
-        question: "Where the famous pizza flavour 'Margherita' was created?",
-        choice1: "Roma",
-        choice2: "Genoa",
-        choice3: "Palermo",
-        choice4: "Napoli",
-        answer: 4  
-    },
-    {
-        question: "Where Gioachino Rossini, the author of the opera 'The Barber of Sevilia' was born?",
-        choice1: "Verona",
-        choice2: "Bologna",
-        choice3: "Pesaro",
-        choice4: "Firenze",
-        answer: 3
-    },
-    {
-        question: "According to tradition, in what year Rome was found?",
-        choice1: "753 BC",
-        choice2: "0",
-        choice3: "476 AC",
-        choice4: "8 BC",
-        answer: 1
-    },
-    {
-        question: "In what italian city the famous story of 'Romeu and Juliet' took place?",
-        choice1: "Venezia",
-        choice2: "Siena",
-        choice3: "Padova",
-        choice4: "Verona",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch(
+    'https://opentdb.com/api.php?amount=50&category=23&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [ ... loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer -1, 
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 
 //Constants
 const CORRECT_BONUS = 10;
@@ -62,8 +56,10 @@ startGame = () => {
     score = 0;
     // spread operator, the 3 dots, spread each of the itens in the questions into a new array
     // we copy so we don't change the original array
-    availableQuestions = [ ... questions];
+    availableQuestions = [ ...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
@@ -81,31 +77,30 @@ getNewQuestion = () => {
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach (choice => {
+    choices.forEach ((choice) => {
         //that's the way we retrieve that data-number set on each choice in game.html
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
     });
 
     //tira a questão já usada para evitar repetições
     availableQuestions.splice(questionIndex, 1);
-
     acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
         if(!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset['number'];
        
         //ternary sintax that compares the answers
         //a comparação é entre string e number, entào double equals
         const classToApply = selectedAnswer==currentQuestion.answer ? "correct" : "incorrect";
         
-        if(classToApply === "correct") {
+        if(classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
 
@@ -121,6 +116,4 @@ choices.forEach(choice => {
 incrementScore = num => {
     score +=num;
     scoreText.innerText = score;
-}
-
-startGame();
+};
